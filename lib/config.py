74c35162e66bf6d6f73d960d83a7018f8e8e5bfc
yunque <469910799@qq.com>
2017-11-14 15:52:23 +0800

    改一下配置文件

--- lib/config.py
+++ lib/config.py
@@ -3,7 +3,7 @@ host_list=['docker_master','docker_node1','docker_node2']
 log_tail_line=300
 #Shows the number of rows from the tail
 #service_name_list=['manage-service','job-service','trace-service','payment-service','message-service','user-service','order-service']
-service_name_list=['manage-service','job-service','trace-service','payment-service','message-service','user-service','order-service']
+service_name_list=['manage','job','trace','payment','message','user','order']
 #A service that serves a timed task to determine whether a service exists
 log_dir_master  = '/log_everyone_bak'
 #Log on the host directory
