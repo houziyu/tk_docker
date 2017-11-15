4037e1b2e2e0d6dab5bc7dc535cd69fb27832385
yunque <469910799@qq.com>
2017-11-15 10:51:06 +0800

    定时任务增加输出信息

--- log/cron_dump_log.py
+++ log/cron_dump_log.py
@@ -18,6 +18,7 @@ def cron_dump_log():
                         log_init = y.get_archive(service_log_path)
                         log_str = str(log_init[0].data, encoding="utf-8")
                         log_local_name = config.log_dir_master+ '/' + service_name + '-service/' + hostname + '-' + service_name + '-' + log_date + '.' + str(num) + '.log'
+                        print(service_log_path)
                         print(log_local_name)
                         log_file = open(log_local_name, 'a+')
                         log_file.write('执行时间:' + log_date)
